234c5fcb0836a06e68043bd986e2969837e76f0d
Utkarsh Yadav <38191717+ViperX7@users.noreply.github.com>
2021-04-04 10:24:37 +0530

    :sparkles: Added some basic modules

--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -4,3 +4,8 @@ local utils = require('utils')
 
 utils.map('n', '<C-l>', '<cmd>noh<CR>') -- Clear highlights
 utils.map('i', 'jk', '<Esc>')           -- jk to escape
+
+-- nvim-tree
+    utils.map('n', '<C-n>', ':NvimTreeToggle<CR>')
+    utils.map('n', '<leader>r', ':<NvimTreeRefreshCR>')
+    utils.map('n', '<leader>n', ':NvimTreeFindFile<CR>')
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,22 +1,25 @@
 return require('packer').startup(function()
 
+----------------------------------------------------------------------
+-- Package Management
+---------------------------------------------------------------------
   -- Packer can manage itself as an optional plugin
     use {'wbthomason/packer.nvim', opt = true}
 
+----------------------------------------------------------------------
+-- Visual Enhancement
+---------------------------------------------------------------------
   -- Color scheme
-    use { 'sainnhe/gruvbox-material'  }
+    use { 'sainnhe/gruvbox-material'}
 
+----------------------------------------------------------------------
+-- Editor Enhancements
+---------------------------------------------------------------------
   -- Fuzzy finder
-    use {
-      'nvim-telescope/telescope.nvim',
-        requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}
-      }
+    use {'nvim-telescope/telescope.nvim',requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}}
 
-  -- LSP and completion
-    use { 'neovim/nvim-lspconfig'  }
-    use { 'nvim-lua/completion-nvim'  }
-
-
+  -- File explorer 
+    use {'kyazdani42/nvim-tree.lua', requires = {{'kyazdani42/nvim-web-devicons'}}}
 
   -- Vim dispatch
     use { 'tpope/vim-dispatch'  }
@@ -25,9 +28,24 @@ return require('packer').startup(function()
     use { 'tpope/vim-fugitive'  }
 
 
+
+----------------------------------------------------------------------
+-- Language Servers
+---------------------------------------------------------------------
+  -- LSP and completion
+    use { 'neovim/nvim-lspconfig'  }
+    use { 'nvim-lua/completion-nvim'  }
+    -- Add symbols to completion menu
+    use {'onsails/lspkind-nvim'}
+
+
+
+
 -----------------------------------------------------
--- Lua development
+-- Language Specific
 -----------------------------------------------------
+
+-- Lua 
     use { 'tjdevries/nlua.nvim'}
       --  better Lua syntax highlighting
         use { 'euclidianAce/BetterLua.vim'}
