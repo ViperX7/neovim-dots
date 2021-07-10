01f4c7df436f111a3e6c8e557296b6a56f5af0b4
Utkarsh Yadav <38191717+ViperX7@users.noreply.github.com>
2021-07-10 13:40:13 +0530

    ⬆️p Udated Plugin Configs

--- lua/config/fluttertools.lua
+++ lua/config/fluttertools.lua
@@ -1,34 +1,46 @@
 -- require("flutter-tools").setup{} -- use defaults
 -- alternatively you can override the default configs
 require("flutter-tools").setup {
-  experimental = { -- map of feature flags
-    lsp_derive_paths = true, -- experimental: Attempt to find the user's flutter SDK
+  ui = {
+    -- the border type to use for all floating windows, the same options/formats
+    -- used for ":h nvim_open_win" e.g. "single" | "shadow" | {<table-of-eight-chars>}
+    border = "rounded",
   },
-  debugger = { -- experimental: integrate with nvim dap
+  debugger = { -- integrate with nvim dap + install dart code debugger
     enabled = false,
   },
   flutter_path = "/opt/flutter/bin/flutter", -- <-- this takes priority over the lookup
   flutter_lookup_cmd = nil, -- example "dirname $(which flutter)" or "asdf where flutter"
   widget_guides = {
     enabled = true,
-    debug = true,
   },
   closing_tags = {
-    highlight = "ErrorMsg",
-    prefix = ">"
+    highlight = "Comment", -- highlight for the closing tag
+    prefix = " // ", -- character to use for close tag e.g. > Widget
+    enabled = true -- set to false to disable
   },
   dev_log = {
-    open_cmd = "tabedit",
+    open_cmd = "tabedit", -- command to use to open the log buffer
+  },
+  dev_tools = {
+    autostart = false, -- autostart devtools server if not detected
+    auto_open_browser = false, -- Automatically opens devtools in the browser
   },
   outline = {
-    open_cmd = "30vnew",
+    open_cmd = "40vnew", -- command to use to open the outline buffer
   },
   lsp = {
     -- on_attach = my_custom_on_attach,
     -- capabilities = my_custom_capabilities -- e.g. lsp_status capabilities
+    --- OR you can specify a function to deactivate or change or control how the config is created
+    capabilities = function(config)
+      config.specificThingIDontWant = false
+      return config
+    end,
     settings = {
       showTodos = true,
-      completeFunctionCalls = true -- NOTE: this is WIP and doesn't work currently
+      completeFunctionCalls = true,
+      -- analysisExcludedFolders = {<path-to-flutter-sdk-packages>}
     }
   }
 }
