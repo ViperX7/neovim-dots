286e9b75b4554c45dd8ce0db1faba0ff1ec9645f
Utkarsh Yadav <38191717+ViperX7@users.noreply.github.com>
2022-08-25 06:24:10 +0530

    Fixed double selection | fix tab on empty places

--- lua/config/nvim-cmp.lua
+++ lua/config/nvim-cmp.lua
@@ -1,11 +1,21 @@
 -- Setup nvim-cmp.
 local cmp = require 'cmp'
 local lspkind = require('lspkind')
+local luasnip = require("luasnip")
 
 -- local luasnip = require("luasnip")
 -- luasnip.snippets = require("luasnip-snippets").load_snippets()
 require("luasnip.loaders.from_vscode").lazy_load()
 
+
+-- Detect if words are before cursor position.
+local has_words_before = function()
+  if vim.api.nvim_buf_get_option(0, 'buftype') == 'prompt' then
+    return false
+  end
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
+end
 
 cmp.setup({
 
@@ -16,15 +26,15 @@ cmp.setup({
     format = lspkind.cmp_format({
       mode = "symbol_text",
       menu = ({
-        -- buffer = "[Buffer]",
-        -- nvim_lsp = "[LSP]",
-        -- luasnip = "[LuaSnip]",
-        -- nvim_lua = "[Lua]",
-        -- latex_symbols = "[Latex]",
+        buffer = "[Buffer]",
+        nvim_lsp = "[LSP]",
+        luasnip = "[LuaSnip]",
+        nvim_lua = "[Lua]",
+        latex_symbols = "[Latex]",
         cmp_tabnine = "[Tabnine]",
         calc = "🖩",
-        -- path = "[Path]",
-        -- emoji = "[emoji]",
+        path = "[Path]",
+        emoji = "[emoji]",
       })
     }),
   },
@@ -36,7 +46,7 @@ cmp.setup({
       -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
       require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
       -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-      vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+      -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
     end,
   },
   window = {
