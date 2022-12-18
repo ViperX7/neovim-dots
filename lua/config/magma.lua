3269424061652bae4a864626069d0609bc71255e
Utkarsh Yadav <38191717+ViperX7@users.noreply.github.com>
2022-12-18 20:15:04 +0530

    added missing configs

---
+++ lua/config/magma.lua
@@ -0,0 +1,4 @@
+vim.g.magma_image_provider = "kitty"
+vim.g.magma_automatically_open_output = false
+vim.g.magma_cell_highlight_group = "run_code"
+vim.api.nvim_set_hl(0, 'run_code', { bg = "#9EC183" })
