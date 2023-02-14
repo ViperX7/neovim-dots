451a4e3b7a79d2e9e3b7a0aa49b74e5b0e899118
Utkarsh Yadav <38191717+ViperX7@users.noreply.github.com>
2023-02-14 22:53:25 +0530

    Colorscheme config and codeium config added

---
+++ lua/config/codeiumvim.lua
@@ -0,0 +1,6 @@
+vim.g.codeium_disable_bindings = 1
+vim.g.codeium_enabled = 0
+vim.keymap.set('i', "<C-r>", function() return vim.fn['codeium#Accept']() end, { expr = true })
+-- vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
+-- vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
+-- vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true })
